34e59314236b21041200f2161e3d4098f3efd5f7
Abhishta <abhishtav.athreya@gmail.com>
2025-09-02 09:30:04 +0530

    Linear Search

--- problem1.py
+++ problem1.py
@@ -1,5 +1,13 @@
-print("Hello World")
-a=3
-b=4
-print(a+b)
-print(a*b)
+def linear_search(arr,target):
+    for i in range(len(arr)):
+        if arr[i]==target:
+            return i+1
+    return -1
+arr=[10,30,40,50,60]
+target=40
+result=linear_search(arr,target)
+if result != -1:
+    print(f"Element {target} found at index {result}")
+else:
+    print(f"Element {target} not found in the list")
+    
